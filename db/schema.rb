2e411a538e008934d3f1faec13d47ccdd34df20b
Dgrady3 <joegrady1@gmail.com>
2015-01-26 21:13:21 -0500

    Fixed bug in status index, now user names show up

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125183516) do
+ActiveRecord::Schema.define(version: 20150127014425) do
 
   create_table "statuses", force: true do |t|
+    t.string   "name"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "user_id"
+    t.integer  "user_id",    null: false
   end
-
-  add_index "statuses", ["user_id"], name: "index_statuses_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "first_name"
