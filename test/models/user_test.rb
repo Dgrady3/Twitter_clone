8bb22543293940b641746fbca34b03aeee18a932
Dgrady3 <joegrady1@gmail.com>
2015-01-28 20:10:32 -0500

    Added test to specify unique profile name

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -22,4 +22,15 @@ class UserTest < ActiveSupport::TestCase
   assert !user.errors[:profile_name].empty?
   end
 
+  test "a user should have a unique profile name" do
+    user = User.new
+    user.profile_name = 'Joe'
+    user.first_name = 'Joe'
+    user.last_name = 'Grady'
+    user.password = 'password'
+    user.password_confirmation ='password'
+    user.email = 'email@email.com'
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
 end
