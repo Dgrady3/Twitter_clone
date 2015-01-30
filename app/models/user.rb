07978d34932fec765d1ff0099192589fb620a4da
Dgrady3 <joegrady1@gmail.com>
2015-01-29 20:14:26 -0500

    Added test and validation for profile name of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :profile_name, presence: true
+  validates :profile_name, presence: true,
+                           uniqueness: true
 
   has_many :statuses
 
