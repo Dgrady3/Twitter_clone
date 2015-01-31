61f65d087616097b8f9942211b335cfc16dd28ff
Dgrady3 <joegrady1@gmail.com>
2015-01-31 15:07:31 -0500

    added tests for status validations

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -11,13 +11,27 @@ class StatusesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:statuses)
   end
 
-  test "should get new" do
+  test "should be redirected when not logged in" do
     get :new
     assert_response :redirect
-    assert_redirect_to new_user_sessions_path
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should render the new page when logged in" do
+    sign_in users(:Joe)
+    get :new
+    assert_response :success
+  end
+
+  test "should be logged in to post a status" do
+    post :create, status: {content: "Hello"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
   end
 
   test "should create status" do
+    sign_in users(:Joe)
+
     assert_difference('Status.count') do
       post :create, status: { content: @status.content, name: @status.name }
     end
@@ -30,17 +44,27 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit when logged in" do
+    sign_in users(:Joe)
+
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
+  test "should redirect status update when not logged in" do
+    put :update, id: @status, status: {content: @status.content}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should update status when logged in" do
+    sign_in users (:Joe)
     patch :update, id: @status, status: { content: @status.content, name: @status.name }
     assert_redirected_to status_path(assigns(:status))
   end
 
   test "should destroy status" do
+    sign_in users(:Joe)
     assert_difference('Status.count', -1) do
       delete :destroy, id: @status
     end
