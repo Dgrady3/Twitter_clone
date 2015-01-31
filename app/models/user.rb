7a50b77bc12a9d74225d6489da61d92a5f0f618d
Dgrady3 <joegrady1@gmail.com>
2015-01-31 15:39:49 -0500

    Removed buddy validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 
 
 
+
   has_many :statuses
 
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -42,4 +42,12 @@ class UserTest < ActiveSupport::TestCase
     assert !user.errors[:profile_name].empty?
     assert user.errors[:profile_name].include?("Must be formatted correctly.")
   end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Joe', last_name: 'Grady', email: 'email@email.com')
+    user.password = user.password_confirmation = 'asdfghjk'
+
+    user.profile_name = 'joe_1'
+    assert user.valid?
+  end
 end
