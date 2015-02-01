5b82cf03da36b035de58eaf19b8261917977e1ca
Dgrady3 <joegrady1@gmail.com>
2015-02-01 13:46:13 -0500

    Added profile tests for proper user, and invalid user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,10 @@
 class ProfilesController < ApplicationController
   def show
+    @user = User.find_by_profile_name(params[:id])
+    if @user
+      render action: :show
+    else
+      render file: 'public/404', status: 404, formats: [:html]
+    end
   end
 end
--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, id: users(:Joe).profile_name
     assert_response :success
+    assert_template 'profiles/show'
   end
 
+  test "should render a 404 on profile not found" do
+    get :show, id: "doesn't exist"
+    assert_response :not_found
+  end
 end
