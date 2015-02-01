92d406e118f0d492587d6256c4ccf4f6872e8e12
Dgrady3 <joegrady1@gmail.com>
2015-02-01 13:57:35 -0500

    Added statuses to show page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   get 'feed', to: 'status#index', as: :feed
   root to: 'statuses#index'
 
+  get '/:id', to: 'profiles#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -11,4 +11,11 @@ class ProfilesControllerTest < ActionController::TestCase
     get :show, id: "doesn't exist"
     assert_response :not_found
   end
+
+  test "that variables are assigned on successful profile viewing" do
+    get :show, id: users(:Joe).profile_name
+    assert assigns(:user)
+    assert_not_empty assigns(:statuses)
+  end
+
 end
