5acfc22d8753406482610d1ae240a1a6f513258f
Dgrady3 <joegrady1@gmail.com>
2015-02-02 18:44:22 -0500

    Added test and code  for current users posting statuses

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -25,7 +25,7 @@ class StatusesController < ApplicationController
   # POST /statuses
   # POST /statuses.json
   def create
-    @status = Status.new(status_params)
+    @status = current_user.statuses.new(status_params)
 
     respond_to do |format|
       if @status.save
