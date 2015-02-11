a3088713e4d2404bdbfab652d039921dc2032de8
Dgrady3 <joegrady1@gmail.com>
2015-02-10 21:59:43 -0500

    Added to user friendship model

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -17,8 +17,9 @@ class UserFriendship < ActiveRecord::Base
       friendship1 = create(user: user1, friend: user2, state: 'pending')
       friendship2 = create(user: user2, friend: user1, state: 'requested')
 
-      friendship1.send_request_email if !friendship1.new_record?
-      friendship1
+      friendship1.accept_mutual_friendship!
+      friendship2.reload
+      assert_equal 'accepted', friendship2.state
     end
   end
 
