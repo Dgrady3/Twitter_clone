55b1627be71a287a4af2f89cf5d84ae2ac2ff559
Dgrady3 <joegrady1@gmail.com>
2015-02-11 16:47:57 -0500

    Tweaked decorator model

--- app/decorators/user_friendship_decorator.rb
+++ app/decorators/user_friendship_decorator.rb
@@ -7,9 +7,10 @@ class UserFriendshipDecorator < Draper::Decorator
 
   def sub_message
     case model.state
-    when 'pending'
-      "Do you really want to be friends with #{model.friend.first_name}?"
-    when 'accepted'
-      "You are friends with #{model.friend.first_name}."
+      when 'pending'
+        "Do you really want to be friends with #{model.friend.first_name}?"
+      when 'accepted'
+        "You are friends with #{model.friend.first_name}."
+    end
   end
 end
