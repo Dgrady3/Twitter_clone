40e2c5e2c4ed7d68e8acc0cace11601e83eec534
Dgrady3 <joegrady1@gmail.com>
2015-02-11 16:47:30 -0500

    Tweaked decorator model

--- app/decorators/user_friendship_decorator.rb
+++ app/decorators/user_friendship_decorator.rb
@@ -2,6 +2,14 @@ class UserFriendshipDecorator < Draper::Decorator
   decorates :user_friendship
 
   def friendship_state
-    "Pending"
+    model.state.titleize
+  end
+
+  def sub_message
+    case model.state
+    when 'pending'
+      "Do you really want to be friends with #{model.friend.first_name}?"
+    when 'accepted'
+      "You are friends with #{model.friend.first_name}."
   end
 end
