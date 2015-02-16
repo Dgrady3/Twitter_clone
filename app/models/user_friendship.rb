bf164f526c5a19a2297325a866c90259957cb08e
Dgrady3 <joegrady1@gmail.com>
2015-02-16 15:57:18 -0500

    Added blocked method to friendship model

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -6,6 +6,8 @@ class UserFriendship < ActiveRecord::Base
 
   state_machine :state, initial: :pending do
     after_transition on: :accept, do: [:send_acceptance_email, :accept_mutual_friendship!]
+    after_transition on: :blocked, do: [:block_mutual_friendship!]
+
 
     state :requested
     state :blocked
@@ -47,5 +49,9 @@ class UserFriendship < ActiveRecord::Base
 
   def delete_mutual_friendship!
     mutual_friendship.delete
+  end
+
+  def block_mutual_friendship!
+    mutual_friendship.update_attribute(:state, 'blocked') if mutual_friendship
   end
 end
